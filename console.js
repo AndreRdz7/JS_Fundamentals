a3e3ed0dd0e28a786790967fcafc5790d9e0a57d
David Andre Rodríguez <David Andre Rodrguez>
2018-05-14 17:22:34 -0500

    console, variables

---
+++ console.js
@@ -0,0 +1,17 @@
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1,b:2});
+console.table({a:1,b:2});
+console.error('This is an error');
+console.clear();
+console.warn('This is a warning');
+console.time('Tables');
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+console.timeEnd('Tables');
---
+++ var_let_const.js
@@ -0,0 +1,44 @@
+//var, let, const
+
+var name = 'Andre';
+console.log(name);
+name = 'David';
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+//letters, numbers, _,$
+//multiword = camel case,underscore,pascal case
+
+var firstName = 'Andre';
+var first_name = 'Andre';
+var FirstName = 'André';
+var firsname = 'nolohagacompa';
+
+//let works the same as var
+let name;
+name = 'Andre';
+console.log(name);
+name = 'David';
+console.log(name);
+
+//const stands for constant, cannot reassing and leaving unassigned
+const name = 'Andre'
+console.log(name);
+
+const person = {
+    name: 'Andre',
+    age: 30
+}
+person.name = 'Sofia';
+person.age = 21;
+
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+
+console.log(numbers);
