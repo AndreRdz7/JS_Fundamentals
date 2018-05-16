828dd45a4121399b4b758037bbb7a71ae70628c6
David Andre Rodríguez <David Andre Rodrguez>
2018-05-15 21:58:44 -0500

    string literals (es6)

---
+++ template_literals.js
@@ -0,0 +1,32 @@
+//template literals
+
+const name = 'John';
+const age = 30;
+const job = 'Web Developer';
+const city = 'Miami';
+let html;
+
+// w/o template strings (es5)
+html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: ' + job + '</li><li>City: ' + city + '</li></ul>';
+
+html = '<ul>' + 
+        '<li>Name: ' + name + '</li>' +
+        'li>Age: ' + age + '</li>' + 
+        '<li>Job: ' + job + '</li>' +
+        '<li>City: ' + city + '</li>' +
+        '</ul>';
+
+
+//with template strings (es6)
+html = `
+    <ul>
+        <li>Name: ${name}</li>
+        <li>Age: ${age}</li>
+        <li>Job: ${job}</li>
+        <li>City: ${city}</li>
+        <li>${2 + 2}</li>
+        <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+    </ul>
+`;
+
+document.body.innerHTML = html;
