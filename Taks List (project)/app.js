5fb555a380acea1c18b69ae79f157e35fbdbe9c6
AndreRdz7 <A01730716@itesm.mx>
2018-06-11 20:41:11 -0500

    button functionality

--- Taks List (project)/app.js
+++ Taks List (project)/app.js
@@ -10,8 +10,14 @@ const taskInput = document.querySelector('#task')
 loadEventListeners();
 
 function loadEventListeners(){
-    //add task event}
+    //add task event
     form.addEventListener('submit',addTask);
+    //event delegation for multiple deleting
+    taskList.addEventListener('click',removeTask);
+    //delete all tasks
+    clearBtn.addEventListener('click',clearTasks);
+    //filter tasks
+    filter.addEventListener('keyup',filterTasks);
 }
 
 //add task
@@ -38,4 +44,35 @@ function addTask(e){
     //clear input
     taskInput.value = '';
     e.preventDefault();
+}
+
+function removeTask(e){
+    //target delete item
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure it\'s done ?')){
+            e.target.parentElement.parentElement.remove();
+        } 
+    }
+}
+
+function clearTasks(e){
+    // option 1
+    // taskList.innerHTML = '';
+    // option 2 (faster)
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(
+    function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
