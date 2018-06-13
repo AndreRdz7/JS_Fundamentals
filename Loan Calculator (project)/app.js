de56e7a0d6f7e52489e282169c2f734668137113
AndreRdz7 <A01730716@itesm.mx>
2018-06-13 01:30:36 -0500

    UX

--- Loan Calculator (project)/app.js
+++ Loan Calculator (project)/app.js
@@ -1,8 +1,15 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    //hide results
+    document.getElementById('results').style.display = 'none';
+    //show loading
+    document.getElementById('loading').style.display = 'block';
+    setTimeout(calculateResults,2000);
+    e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e){
+function calculateResults(){
     // UI Vars
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
@@ -24,14 +31,21 @@ function calculateResults(e){
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+        //show results
+        document.getElementById('results').style.display = 'block';
+        //hide loading
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please check your numbers');
     }
-    e.preventDefault();
 }
 
 // Show Error
 function showError(error){
+    //hide results
+    document.getElementById('results').style.display = 'nove';
+    //hide loading
+    document.getElementById('loading').style.display = 'none';
     // Create a div
     const errorDiv = document.createElement('div');
     // Get elements
