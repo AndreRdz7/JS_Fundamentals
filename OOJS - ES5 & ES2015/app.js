04afd0f6caf5ba86e1fa4fca58f9f702cb2b134e
AndreRdz7 <A01730716@itesm.mx>
2018-06-14 20:24:27 -0500

    Generic constructors

--- OOJS - ES5 & ES2015/app.js
+++ OOJS - ES5 & ES2015/app.js
@@ -1,34 +1,45 @@
-//ok for one object
-/*
-const brad = {
-    name: 'Brad',
-    age: 30
-}
-console.log(brad);
-*/
+//built in constructors
 
-//multiple instances
-//constructor
-function Person(name,dob){
-    this.name = name;
-    // scope = constructor - object
-    // console.log(this);
-    this.birthday = new Date(dob);
-    this.calculateAge = function(){
-        const diff = Date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
+//string
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
+name2.foo = 'bar';
+console.log(name1);
+console.log(name2);
+console.log(typeof name2);
+
+//not the same type
+if(name2 === 'Jeff'){
+    console.log('yes');
+} else {
+    console.log('no');
 }
 
-// scope = global - window
-// console.log(this);
+//number
+const num1 = 5;
+const num2 = new Number(5);
 
+//booleans
+const bool1 = true;
+const bool2 = new Boolean(true);
 
-const brad = new Person('Brad','9/10/1981');
-console.log(brad);
-console.log(brad.calculateAge());
+//functions
+const getSum1 = function(x,y){
+    return x + y;
+}
+console.log(getSum1(1,1));
+const getSum2 = new Function('x','y','return 1 + 1');
+console.log(getSum2(1,1));
 
+//objects
+const john1 = {name:"john"};
+const john2 = new Object({name:"john"});
 
+//arrays
+const arr1 = [1,2,3,4];
+const arr2 = new Array(1,2,3,4);
 
- 
+//regex
+const re1 = /\w+/;
+const re2 = new RegExp('\\w+');
+console.log(re2);
