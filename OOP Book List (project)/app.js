a8607a0694592ee37d6ca95b6356cbbc190f7b23
AndreRdz7 <A01730716@itesm.mx>
2018-06-15 23:51:22 -0500

    validation and alerts

--- OOP Book List (project)/app.js
+++ OOP Book List (project)/app.js
@@ -8,6 +8,7 @@ function Book(title,author,isbn){
 //ui constructor
 
 function UI() {}
+
 UI.prototype.addBooktoList = function(book){
     const list = document.getElementById('book-list');
     //create tr
@@ -22,10 +23,27 @@ UI.prototype.addBooktoList = function(book){
     list.appendChild(row);
 }
 
-UI.prototype.clearFields = funcion(){
-    document.getElementById('title') = '';
-    document.getElementById('author') = '';
-    document.getElementById('isbn') = '';
+UI.prototype.clearFields = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+UI.prototype.showAlert = function(message,className){
+    //create div
+    const div = document.createElement('div');
+    //add class
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    //get parent
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    //insert alert
+    container.insertBefore(div,form);
+    //timeout after 3 secs
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    },3000);
 }
 
 //event listener
@@ -36,9 +54,18 @@ document.getElementById('book-form').addEventListener('submit',function(e){
     const book = new Book(title,author,isbn);
     console.log(book);
     const ui = new UI();
-    //add book to list
-    ui.addBooktoList(book);
-    //clear field
-    ui.clearFields();
+    //validate
+    if(title === '' || author === '' || isbn === ''){
+        //error alert
+        ui.showAlert('Please fill all fields','error');
+    } else {
+        //add book to list
+        ui.addBooktoList(book);
+        //show success
+        ui.showAlert('Book Added!','success')
+        //clear field
+        ui.clearFields();
+    }
+    
     e.preventDefault();
 });
