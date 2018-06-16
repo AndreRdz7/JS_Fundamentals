0de8e05f33ed45ce2d685448dc03d36313cc5bcf
AndreRdz7 <A01730716@itesm.mx>
2018-06-16 11:41:06 -0500

    migrated form es5 prototypes to es6 classes

--- OOP Book List (project)/app.js
+++ OOP Book List (project)/app.js
@@ -15,10 +15,10 @@ UI.prototype.addBooktoList = function(book){
     const row = document.createElement('tr');
     //insert cols
     row.innerHTML = `
-        <tr>${book.title}</tr>
-        <tr>${book.author}</tr>
-        <tr>${book.isbn}</tr>
-        <tr><a href="#" class="delete">X<a></tr>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X<a></td>
     `;
     list.appendChild(row);
 }
