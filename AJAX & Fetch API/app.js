b6ed22031e049ea1bff476ea33b0caab9ad57aeb
AndreRdz7 <A01730716@itesm.mx>
2018-06-17 20:07:10 -0500

    callback functions

--- AJAX & Fetch API/app.js
+++ AJAX & Fetch API/app.js
@@ -1,27 +1,56 @@
-document.querySelector('.get-jokes').addEventListener('click',getJokes);
-
-function getJokes(e){
-    const number = document.querySelector('input[type="number"]').value;
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`,true);
-    xhr.onload = function(){
-        if(xhr.status === 200){
-            const response = JSON.parse(this.responseText);
-            console.log(response);
-            let output = '';
-            if(response.type === 'success'){
-                response.value.forEach(function(joke){
-                    output += `
-                    <li>${joke.joke}</li>
-                    `
-                });
-            } else {
-                output += '<li>Something went wrong</li>';
-            }
-
-            document.querySelector('.jokes').innerHTML = output;
-        }
+const posts = [
+    {
+       title: 'Post One',
+       body: 'This is post one' 
+    },
+    {
+        title: 'Post Two',
+        body: 'This is post two'
     }
-    xhr.send();
-    e.preventDefault();
+];
+
+/*
+
+Async
+
+
+function createPost(post){
+    setTimeout(function(){
+        posts.push(post)
+    },2000);
 }
+
+function getPosts(){
+    setTimeout(function(){
+        let output = '';
+        posts.forEach(function(post){
+            output += `<li>${post.title}<li>`
+        });
+        document.body.innerHTML = output;
+    },1000)
+}
+
+createPost({title: 'Post Three',body: 'This is post three'});
+
+getPosts();
+
+*/
+
+function createPost(post, callback){
+    setTimeout(function(){
+        posts.push(post);
+        callback();
+    },2000);
+}
+
+function getPosts(){
+    setTimeout(function(){
+        let output = '';
+        posts.forEach(function(post){
+            output += `<li>${post.title}<li>`
+        });
+        document.body.innerHTML = output;
+    },1000)
+}
+
+createPost({title: 'Post Three',body: 'This is post three'}, getPosts);
