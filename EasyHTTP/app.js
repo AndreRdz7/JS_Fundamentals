96413fa4fbecdea5475ab678aa89512c4abe2d8b
AndreRdz7 <A01730716@itesm.mx>
2018-06-18 20:44:34 -0500

    post request

--- EasyHTTP/app.js
+++ EasyHTTP/app.js
@@ -1,10 +1,29 @@
 const http = new easyHTTP;
 
 //get posts
+// get 1 post -> posts/1 and use singular con every other posts
+/*
+code works fine
 http.get('https://jsonplaceholder.typicode.com/posts',function(err,posts){
     if(err){
         console.log(err);
     } else {
         console.log(posts);
+    }
+
+});
+*/
+//post request
+//create data
+const data = {
+    title: 'Custom post',
+    body: 'hi'
+};
+
+http.post('https://jsonplaceholder.typicode.com/posts',data,function(err,post){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(post);
     }
 });
