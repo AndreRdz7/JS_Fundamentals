83b220af7a9b64ff20d07cd059f6b00cddac22ab
AndreRdz7 <A01730716@itesm.mx>
2018-06-18 20:50:36 -0500

    delete request

--- EasyHTTP/app.js
+++ EasyHTTP/app.js
@@ -31,10 +31,21 @@ http.post('https://jsonplaceholder.typicode.com/posts',data,function(err,post){
 */
 
 //update post (put)
+/* code works
 http.put('https://jsonplaceholder.typicode.com/posts/1',data,function(err,post){
     if(err){
         console.log(err);
     } else {
         console.log(postS);
     }
+});
+*/
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1',function(err,response){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(response);
+    }
+
 });
--- EasyHTTP/easyhttp.js
+++ EasyHTTP/easyhttp.js
@@ -28,7 +28,7 @@ easyHTTP.prototype.post = function(url,data,callback){
     this.http.send(JSON.stringify(data));
 }
 //HTTP PUT Request
-easyHTTP.prototype.post = function(url,data,callback){
+easyHTTP.prototype.put = function(url,data,callback){
     this.http.open('PUT',url,true);
     this.http.setRequestHeader('Content-type','application/json');
     let self = this;
@@ -40,3 +40,15 @@ easyHTTP.prototype.post = function(url,data,callback){
     this.http.send(JSON.stringify(data));
 }
 //HTTP DELETE Request
+easyHTTP.prototype.delete = function(url,callback){
+    this.http.open('DELETE',url,true);
+    let self = this;
+    this.http.onload = function(){
+        if(self.http.status === 200){
+            callback(null,'Post deleted');
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    }
+    this.http.send();
+}
