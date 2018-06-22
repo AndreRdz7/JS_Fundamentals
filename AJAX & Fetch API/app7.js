ca5f2d9504f6a3de7a283484387c3c342d875f88
AndreRdz7 <A01730716@itesm.mx>
2018-06-21 22:59:14 -0500

    Async and await, manipulating promises

---
+++ AJAX & Fetch API/app7.js
@@ -0,0 +1,41 @@
+const http = new EasyHTTP;
+
+//get users 
+//no promise
+/*
+http.get('https://jsonplaceholder.typicode.com/users');
+*/
+//with promise
+
+/*
+http.get('https://jsonplaceholder.typicode.com/users')
+.then(data => console.log(data))
+.catch(err => console.log(err));
+*/
+
+//user data
+const data = {
+    name: 'John',
+    username: 'SuperJ',
+    email: 'hey@meh.com'
+}
+//create post
+/*
+http.post('https://jsonplaceholder.typicode.com/users', data)
+.then(data => console.log(data))
+.catch(err => console.log(err));
+*/
+
+//update post
+http.put('https://jsonplaceholder.typicode.com/users/1', data)
+.then(data => console.log(data))
+.catch(err => console.log(err));
+
+//delete post
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+.then(data => console.log(data))
+.catch(err => console.log(err));
+
+http.get('https://jsonplaceholder.typicode.com/users')
+.then(data => console.log(data))
+.catch(err => console.log(err));
