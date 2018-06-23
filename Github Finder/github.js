feef70b13378386fabc74156640324ab4c8663e2
AndreRdz7 <A01730716@itesm.mx>
2018-06-22 23:52:49 -0500

    repos list added

--- Github Finder/github.js
+++ Github Finder/github.js
@@ -2,15 +2,20 @@ class Github{
     constructor(){
         this.client_id = '6f153f923ed90f4f3ebf';
         this.client_secret = '0f01353ae5ed803b8d8b5de90c51885fd4a0f79d';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
         
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return{
-            profile 
+            profile,
+            repos 
         }
     }
 }
