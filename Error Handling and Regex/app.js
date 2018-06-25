d21bd0d090942ef25c3fb85c147f66c1af846076
AndreRdz7 <A01730716@itesm.mx>
2018-06-25 13:53:25 -0500

    Char sets, quantifiers and grouping regex

--- Error Handling and Regex/app.js
+++ Error Handling and Regex/app.js
@@ -12,8 +12,29 @@ re = /h*d/i;     // ignores whats in between (all)
 re = /gre?a?y/;  // optional characters (any of them or none at all)
 re = /gray\?/;   // scape characters
 
+//Brackets [] - Character sets
+re = /gr[ae]y/i;   // must be a or e, but not null
+re = /[GF]ray/;    // must be G or F, but not null
+re = /[^GF]ray/;   // match anything except G or F
+re = /^[GF]ray/;   // match if starts with G or F
+re = /[A-Z]ray/;   // match any uppercase
+re = /[a-z]ray/;   // match any lowercase
+re = /[A-Za-z]ray/;// match any
+re = /[0-9]ray/;   // match any number
+
+//Braces {} - Quantifiers
+re = /Hel{2}o/i;   // looks for exact two l
+re = /Hel{2,4}o/i; // looks for two to four l
+re = /Hel{2,}o/i;  // looks for att least two l
+
+//Parenthesis () - Grouping
+re = /^([0-9]x){3}$/; //looks for ex: 3x3x3x, 1x2x3x
+
+
+
+
 //string to match
-const str = 'Hello World';
+const str = 'Hello World'; 
 
 //log results
 const result = re.exec(str);
